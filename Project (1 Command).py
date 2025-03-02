bdde4ae943ff4c458254b65603d28b06c4b95542
vanil_2012TEPMuHATOP <nika010vip@gmail.com>
2025-03-02 17:14:18 +0200

    Update Project (1 Command).py

---
+++ Project (1 Command).py
@@ -0,0 +1,25 @@
+from pygame import *
+from random import randint
+
+class GameSprite(sprite.Spite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 650:
+            self.rect.y += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.y < 450:
+            self.rect.y += self.speed
